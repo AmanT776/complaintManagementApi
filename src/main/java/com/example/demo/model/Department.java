d9d8d351dc6a595a8ad27cf2dc6e6a934d70aeb8
AmanT776 <amantes776@gmail.com>
2025-12-01 20:21:17 +0300

    feat:-configured one to many relationship with user and department

--- src/main/java/com/example/demo/model/Department.java
+++ src/main/java/com/example/demo/model/Department.java
@@ -1,19 +1,17 @@
 package com.example.demo.model;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import jakarta.persistence.Id;
 
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -29,4 +27,7 @@ public class Department {
     private LocalDateTime createdAt;
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+    @OneToMany
+    @JoinColumn(name = "department_id")
+    private List<User> users = new ArrayList<>();
 }
